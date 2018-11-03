82aaa44c53fdc847c5a3f13b3df648ab55685aab
jbae20 <fxcence@gmail.com>
2018-11-03 00:58:24 -0400

    almost

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import {getFunName} from "../helpers"
+import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
-goToStore(event){
-  event.preventDefault();
-  console.log(`going to store`);
-}
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.myInput.current.value);
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`)
+  };
   render() {
     return (
-      <React.Fragment>
-        <form action="" className="store-selector" onSubmit={this.goToStore}>
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A store</h2>
-          <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+          <input type="text"
+            ref={this.myInput}
+            required placeholder="Store Name" defaultValue={getFunName()} />
           <button type="submit"> Visit Store → </button>
         </form>
-      </React.Fragment>
     )
   }
 }
